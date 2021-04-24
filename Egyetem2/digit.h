f3e7d1e13eeb5342828d065937e54c35cbb296b3
kernzsolt <tadrem@gmail.com>
2021-04-24 15:54:25 +0200

    Digit

--- Egyetem2/digit.h
+++ Egyetem2/digit.h
@@ -6,7 +6,7 @@ private:
 	segment* seg[7];
 	int x, y;
 	//0bHGFEDCBA
-	unsigned char digits[10] = {54, 88, 0b01011011, 44, 55, 66, 22,  93, 123, 200};
+	unsigned char digits[10] = {0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101,  0b00000111, 0b01111111, 0b01101111};
 public:
 	digit(const int& x, const int& y);
 	void set(int num);
